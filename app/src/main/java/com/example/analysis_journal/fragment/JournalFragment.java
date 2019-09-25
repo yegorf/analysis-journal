0b51c39ebc7d9f25a73812a3d091be1d766d3403
yegorf <yegorkhohlov@gmail.com>
2019-09-25 23:31:30 +0300

    Refactoring and redesign

--- app/src/main/java/com/example/analysis_journal/fragment/JournalFragment.java
+++ app/src/main/java/com/example/analysis_journal/fragment/JournalFragment.java
@@ -6,24 +6,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import com.example.analysis_journal.R;
 import com.example.analysis_journal.navigation.NavigationManager;
 
+import androidx.annotation.BinderThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class JournalFragment extends Fragment {
 
     @BindView(R.id.btn_add_analyse)
     public Button addAnalyseBtn;
 
+    @BindView(R.id.rv_journal)
+    public RecyclerView journalRecycler;
+
+    @OnClick(R.id.btn_add_analyse)
+    void redirectAddAnalyse() {
+        NavigationManager manager = new NavigationManager(getFragmentManager());
+        manager.openFragment(NavigationManager.SCREEN_ADD_RESULT);
+    }
+
     public static JournalFragment newInstance() {
-        JournalFragment fragment = new JournalFragment();
-        return fragment;
+        return new JournalFragment();
     }
 
     @Nullable
@@ -36,9 +46,6 @@ public class JournalFragment extends Fragment {
     }
 
     private void initViews() {
-        addAnalyseBtn.setOnClickListener(e -> {
-            NavigationManager manager = new NavigationManager(getFragmentManager());
-            manager.openFragment(NavigationManager.SCREEN_ADD_RESULT);
-        });
+
     }
 }
--- app/src/main/java/com/example/analysis_journal/fragment/NavigationFragment.java
+++ app/src/main/java/com/example/analysis_journal/fragment/NavigationFragment.java
@@ -4,34 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.widget.Button;
 
 import com.example.analysis_journal.R;
 import com.example.analysis_journal.navigation.NavigationManager;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NavigationFragment extends Fragment {
 
     @BindView(R.id.ib_journal)
-    public ImageButton journalBtn;
+    public Button journalBtn;
 
     @BindView(R.id.ib_info)
-    public ImageButton infoBtn;
+    public Button infoBtn;
 
     @BindView(R.id.ib_statistic)
-    public ImageButton statisticBtn;
+    public Button statisticBtn;
 
     @BindView(R.id.ib_document)
-    public ImageButton documentBtn;
+    public Button documentBtn;
 
-    NavigationFragment getInstance() {
-        return new NavigationFragment();
+    @OnClick(R.id.ib_journal)
+    void redirectJournal() {
+        NavigationManager manager = new NavigationManager(getFragmentManager());
+        manager.openFragment(NavigationManager.SCREEN_JOURNAL);
+    }
+
+    @OnClick(R.id.ib_info)
+    void redirectInfo() {
+        NavigationManager manager = new NavigationManager(getFragmentManager());
+        manager.openFragment(NavigationManager.SCREEN_INFO);
     }
 
     @Nullable
@@ -39,19 +47,6 @@ public class NavigationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigation, container, false);
         ButterKnife.bind(this, view);
-        initViews();
         return view;
-    }
-
-    private void initViews() {
-        journalBtn.setOnClickListener(e -> {
-            NavigationManager manager = new NavigationManager(getFragmentManager());
-            manager.openFragment(NavigationManager.SCREEN_JOURNAL);
-        });
-
-        infoBtn.setOnClickListener(e -> {
-            NavigationManager manager = new NavigationManager(getFragmentManager());
-            manager.openFragment(NavigationManager.SCREEN_INFO);
-        });
     }
 }
